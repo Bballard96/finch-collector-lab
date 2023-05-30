e44b2b8bfc04070eace2fd9990182234ad3205a6
Brendan96 <Brendan.ballard96@yahoo.com>
2023-05-30 14:31:55 -0500

    changes for part 2

---
+++ main_app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+
+# Add the Cat class & list and view function below the imports
+class Finch:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+finches = [
+  Finch('Lolo', 'tabby', 'Kinda rude.', 3),
+  Finch('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Finch('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Finch('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finch_index(request):
+  return render(request, 'finches/index.html', { 'finches': finches})
+
+def home(request):
+  return render(request, 'home.html')
